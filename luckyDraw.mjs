3463962d6940604de50d2db64d283a8868470974
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-02-08 18:08:50 +0100

    exercise await LuckyDraw

---
+++ luckyDraw.mjs
@@ -0,0 +1,39 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+// const players = ['Joe', 'Caroline', 'Sabrina']
+// function LuckyDraw(player){
+//    luckyDraw(player)
+//    .then((value) => console.log(value))
+//    .catch((err) => console.error(err))
+// }
+
+// players.forEach((player) => {
+//    LuckyDraw(player)
+// })
+
+function getResults() {
+  const players = ["Joe", "Caroline", "Sabrina"];
+  players.forEach(async (player) => {
+    try {
+      const result = await luckyDraw(player);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+
+
+getResults()
